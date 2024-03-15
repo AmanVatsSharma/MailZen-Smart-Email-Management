25f61b3718b944d7f9dbd680c134afc4d132c121
AmanVatsSharma <aman95026@gmail.com>
2024-03-15 17:07:38 +0530

    feat(organization): Add label module and entity

---
+++ backend/src/organization/label.entity.ts
@@ -0,0 +1,13 @@
+import { Field, ObjectType, ID } from '@nestjs/graphql';
+
+@ObjectType()
+export class Label {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field({ nullable: true })
+  color?: string; // Optional: to style the label
+} 
---
+++ backend/src/organization/label.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { LabelService } from './label.service';
+import { LabelResolver } from './label.resolver';
+
+@Module({
+  providers: [LabelService, LabelResolver],
+  exports: [LabelService],
+})
+export class LabelModule {} 
